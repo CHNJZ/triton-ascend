f96b1ad182f0d2633a87b5a85a975d62ab7e8c7d
chnjz233 <jiangzheng26@huawei.com>
2026-07-27 20:30:19 +0800

    [Backend](fix) Support Ascend910_9363
    Signed-off-by: chnjz233 <jiangzheng26@huawei.com>

--- third_party/ascend/backend/utils.py
+++ third_party/ascend/backend/utils.py
@@ -535,6 +535,7 @@ def get_ascend_arch_from_env():
         "Ascend910B3",
         "Ascend910B4",
         "Ascend910_9362",
+        "Ascend910_9363",
         "Ascend910_9372",
         "Ascend910_9381",
         "Ascend910_9382",
